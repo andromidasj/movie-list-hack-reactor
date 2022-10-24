f4d9c0ad743d9ce07b273aa4979ec3dab9818e60
Andromidas, Josh [USA] <Andromidas_Joshua@bah.com>
2022-10-24 16:39:33 -0500

    add types for .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TRAKT_API_KEY: string;
+  readonly VITE_TRAKT_SECRET: string;
+  readonly VITE_TMDB_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
