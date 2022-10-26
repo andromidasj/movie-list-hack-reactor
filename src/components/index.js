078a4e2433f86bec04552f2859c422732e0aa7cb
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2022-10-26 11:20:19 -0500

    Delete index.js

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ListActions } from './ListActions/ListActions';
-export { default as SearchBar } from './SearchBar/SearchBar';
-export { default as Tabs } from './Tabs/Tabs';
-export { default as TitleNav } from './TitleNav/TitleNav';
-export { default as WatchProviders } from './WatchProviders/WatchProviders';
