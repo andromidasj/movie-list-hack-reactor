c59c45e6d6b65248048298d466be9cab8432e55f
Andromidas, Josh [USA] <Andromidas_Joshua@bah.com>
2022-11-01 14:54:43 -0500

    remove aspect ratio formatting

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -20,6 +20,7 @@ const MOVIE_PATH = '/movie';
 const POSTER_PATH = 'https://www.themoviedb.org/t/p/w342';
 const IMG_RATIO = 2 / 3;
 const CARD_HEIGHT = 170;
+const CARD_WIDTH = 113;
 
 function MovieCard({
   tmdbId,
@@ -48,17 +49,13 @@ function MovieCard({
 
     return (
       <Link to={{ pathname, search }} style={{ textDecoration: 'none' }}>
-        <AspectRatio ratio={IMG_RATIO} sx={{ height: CARD_HEIGHT }}>
-          <Image
-            height={CARD_HEIGHT}
-            fit="cover"
-            radius="md"
-            alt={`${movie.title} movie poster`}
-            withPlaceholder
-            ref={ref}
-            src={urlJoin(POSTER_PATH, movie.posterPath)}
-          />
-        </AspectRatio>
+        <Image
+          radius="md"
+          alt={`${movie.title} movie poster`}
+          withPlaceholder
+          ref={ref}
+          src={urlJoin(POSTER_PATH, movie.posterPath)}
+        />
         <Text
           color="white"
           transform="uppercase"
